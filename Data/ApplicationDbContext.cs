52de8d711cd0f48ace84e82b07436bfbfe9aa447
Lucas da Silva Pereira <lucas.pereira-cs@interfile.com.br>
2018-04-18 18:46:35 -0300

    Continuaçã - pg 60

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -22,5 +22,7 @@ namespace AspNetCoreTodo.Data
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
         }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
     }
 }
---
+++ Services/FakeTodoItemService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreTodo.Models;
+
+namespace AspNetCoreTodo.Services
+{
+    public class FakeTodoItemService : ITodoItemService
+    {
+        public Task<IEnumerable<TodoItem>> GetIncompleteItemsAsync()
+        {
+            IEnumerable<TodoItem> items = new[]
+            {
+                new TodoItem
+                {
+                    Title = "Learn .NET Core",
+                    DueAt = DateTimeOffset.Now.AddDays(3)
+                },
+                new TodoItem
+                {
+                    Title = "Learn ES6",
+                    DueAt = DateTimeOffset.Now.AddDays(5)
+                },
+                new TodoItem
+                {
+                    Title = "Learn React",
+                    DueAt = DateTimeOffset.Now.AddDays(9)
+                }
+            };
+
+            return Task.FromResult(items);
+        }
+    }
+}
